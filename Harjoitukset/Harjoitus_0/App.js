3e8c21831344adf5aaac68b45b98652d0915dc74
ihaap <89947081+ihaap@users.noreply.github.com>
2024-08-19 23:00:32 +0300

    Lisätty Harjoitus_0

---
+++ Harjoitukset/Harjoitus_0/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 20 , color: "white"}}>Testing testing. 123 123.
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
